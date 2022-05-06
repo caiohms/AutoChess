7c6c3ed9c9bfa988ca57248d9e3200bc326d6c97
Alex Cohen <alex-dambros@hotmail.com>
2022-05-06 16:47:22 -0300

    minimax

--- AI.cpp
+++ AI.cpp
@@ -7,15 +7,32 @@ AI::AI(ChessBoard &board) : board(board) {}
 
 double AI::minimax(ChessBoard chessBoard, int depth, double alpha, double beta, PlayerTurn playerTurn) {
 
-//    playerTurn = (playerTurn == WHITE) ? BLACK : WHITE;
-
 //    if (depth == 0) ||
+    double eval;
 
-//    if (playerTurn) // Black
-    double maxEval = std::numeric_limits<double>::infinity();
-    std::cout << maxEval << std::endl;
+    if (playerTurn == PlayerTurn::BLACK) { // Black
+        double maxEval = -std::numeric_limits<double>::infinity();
+        for (int i = 0; i < 64; ++i) {
+            eval = minimax(, depth - 1, alpha, beta, WHITE);
+            maxEval = std::max(maxEval, eval);
+            alpha = std::max(alpha, eval);
 
-    return maxEval;
+            if (beta <= alpha)break;
+
+        }
+        return maxEval;
+    } else if (playerTurn == PlayerTurn::WHITE) { // WHITE
+        double minEval = std::numeric_limits<double>::infinity();
+        for (int i = 0; i < 64; ++i) {
+            eval = minimax(, depth - 1, alpha, beta, BLACK);
+            minEval = std::min(minEval, eval);
+            alpha = std::min(alpha, eval);
+
+            if (beta <= alpha)break;
+
+        }
+        return minEval;
+    }
 }
 
 void AI::doEvaluate() {
@@ -24,6 +41,7 @@ void AI::doEvaluate() {
                                                  board.getEnPassantEnabledSquare(), board.getSquares());
 
     std::cout << "Evaluating board" << std::endl;
+    double result = minimax(board, 2, 0, 0, BLACK);
 
 
 }
